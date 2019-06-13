50b01b25f097988b49d0300dae1067966a3a7194
hmm <hmmreborn@163.com>
2019-06-13 15:12:45 +0800

    city ajax

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -4,17 +4,8 @@
       <div class="area">
         <div class="title border-topbottom">当前城市</div>
         <div class="button-list">
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">北京</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">北京</div>
+          <div class="button-wrapper" v-for="item in hot" :key="item.id">
+            <div class="button">{{item.name}}</div>
           </div>
         </div>
       </div>
@@ -32,26 +23,10 @@
           </div>
         </div>
       </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
+      <div class="area" v-for="(item, key) in cities" :key="key">
+        <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
-          <div class="item  border-bottom">哈尔滨</div>
-          <div class="item  border-bottom">哈尔滨</div>
-          <div class="item  border-bottom">哈尔滨</div>
-          <div class="item  border-bottom">哈尔滨</div>
-          <div class="item  border-bottom">哈尔滨</div>
-          <div class="item  border-bottom">哈尔滨</div>
-        </div>
-      </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-          <div class="item  border-bottom">哈尔滨</div>
-          <div class="item  border-bottom">哈尔滨</div>
-          <div class="item  border-bottom">哈尔滨</div>
-          <div class="item  border-bottom">哈尔滨</div>
-          <div class="item  border-bottom">哈尔滨</div>
-          <div class="item  border-bottom">哈尔滨</div>
+          <div class="item  border-bottom" v-for="innerItem in item" :key="innerItem.id">{{innerItem.name}}</div>
         </div>
       </div>
     </div>
@@ -62,6 +37,10 @@
 import BScroll from 'better-scroll'
 export default {
   name: 'CityList',
+  props: {
+    hot: Array,
+    cities: Object
+  },
   mounted () {
     this.scroll = new BScroll(this.$refs.wrapper)
   }
